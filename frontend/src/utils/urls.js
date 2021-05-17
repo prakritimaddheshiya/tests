5af3703eb050100a3b79122a91725ed00ce23d68
Ayush <ayushgupta0010@gmail.com>
2021-05-17 15:53:24 +0530

    build react

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,5 @@
-export const TEST_CREATE = "https://prakritimaddheshiya.herokuapp.com/tests/create";
+export const TEST_CREATE =
+  "https://prakritimaddheshiya.herokuapp.com/tests/create";
 export const TEST_LIST = "https://prakritimaddheshiya.herokuapp.com/tests/list";
-export const TEST_DETAIL = "https://prakritimaddheshiya.herokuapp.com/tests/detail/";
+export const TEST_DETAIL =
+  "https://prakritimaddheshiya.herokuapp.com/tests/detail/";
